fb6c6800743360ad3eb0c9234b85548f32260962
Bagas Hizbullah <work.bitsy@simplelogin.com>
2023-08-14 17:12:18 +0700

    feat: authorizations

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -22,8 +22,13 @@ class NotesHandler {
         try {
             this._validator.validateNotePayload(request.payload);
             const { title = 'untitled', body, tags } = request.payload;
-
-            const noteId = await this._service.addNote({ title, body, tags });
+            const { id: credentialId } = request.auth.credentials;
+            const noteId = await this._service.addNote({
+                title,
+                body,
+                tags,
+                owner: credentialId,
+            });
 
             const response = h.response({
                 status: 'success',
@@ -58,8 +63,9 @@ class NotesHandler {
     /**
      * getNotesHandler is a method that will be used to handle the GET request to get all notes
      */
-    async getNotesHandler() {
-        const notes = await this._service.getNotes();
+    async getNotesHandler(request) {
+        const { id: credentialId } = request.auth.credentials;
+        const notes = await this._service.getNotes(credentialId);
         return {
             status: 'success',
             data: {
@@ -75,7 +81,11 @@ class NotesHandler {
     async getNoteByIdHandler(request, h) {
         try {
             const { id } = request.params;
+            const { id: credentialId } = request.auth.credentials;
+
+            await this._service.verifyNoteOwner(id, credentialId);
             const note = await this._service.getNoteById(id);
+
             return {
                 status: 'success',
                 data: {
@@ -111,7 +121,8 @@ class NotesHandler {
         try {
             this._validator.validateNotePayload(request.payload);
             const { id } = request.params;
-
+            const { id: credentialId } = request.auth.credentials;
+            await this._service.verifyNoteOwner(id, credentialId);
             await this._service.editNoteById(id, request.payload);
 
             return {
@@ -146,6 +157,8 @@ class NotesHandler {
     async deleteNoteByIdHandler(request, h) {
         try {
             const { id } = request.params;
+            const { id: credentialId } = request.auth.credentials;
+            await this._service.verifyNoteOwner(id, credentialId);
             await this._service.deleteNoteById(id);
 
             return {
--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -3,6 +3,7 @@ const { Pool } = require('pg');
 const InvariantError = require('../../exceptions/InvariantError');
 const { mapDBToModel } = require('../../utils');
 const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthorizationError = require('../../exceptions/AuthorizationError');
 
 /**
  * NotesService is a class that will be used to handle all of the CRUD operations on notes data
@@ -15,14 +16,15 @@ class NotesService {
     /**
      * addNote is a method that will be used to handle the POST request to add a note
      */
-    async addNote({ title, body, tags }) {
+    // eslint-disable-next-line object-curly-newline
+    async addNote({ title, body, tags, owner }) {
         const id = nanoid(16);
         const createdAt = new Date().toISOString();
         const updatedAt = createdAt;
 
         const query = {
-            text: 'INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
-            values: [id, title, body, tags, createdAt, updatedAt],
+            text: 'INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id',
+            values: [id, title, body, tags, createdAt, updatedAt, owner],
         };
 
         const result = await this._pool.query(query);
@@ -37,9 +39,36 @@ class NotesService {
     /**
      * getNotes is a method that will be used to handle the GET request to get all notes
      */
-    async getNotes() {
-        const result = await this._pool.query('SELECT * FROM notes');
+    async getNotes(owner) {
+        const query = {
+            text: 'SELECT * FROM notes WHERE owner = $1',
+            values: [owner],
+        };
+        const result = await this._pool.query(query);
         return result.rows.map(mapDBToModel);
+    }
+
+    /**
+     * verifyNoteOwner is a method that will be used to handle the GET request
+     * to verify the owner of a note
+     */
+    async verifyNoteOwner(id, owner) {
+        const query = {
+            text: 'SELECT * FROM notes WHERE id = $1',
+            values: [id],
+        };
+
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new NotFoundError('Resource yang Anda minta tidak ditemukan');
+        }
+
+        const note = result.rows[0];
+
+        if (note.owner !== owner) {
+            throw new AuthorizationError('Anda tidak berhak mengakses resource ini');
+        }
     }
 
     /**
