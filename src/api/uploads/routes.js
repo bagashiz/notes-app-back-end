e9750c88d6d99a648cc4520c90954b5c2804fa77
Bagas Hizbullah <work.bitsy@simplelogin.com>
2023-09-03 22:00:54 +0700

    feat: implement local storage

---
+++ src/api/uploads/routes.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+/**
+ * routes handles the routes for the uploads API.
+ */
+const routes = (handler) => [
+    {
+        method: 'POST',
+        path: '/upload/images',
+        handler: handler.postUploadImageHandler,
+        options: {
+            payload: {
+                allow: 'multipart/form-data',
+                multipart: true,
+                output: 'stream',
+            },
+        },
+    },
+    {
+        method: 'GET',
+        path: '/upload/{param*}',
+        handler: {
+            directory: {
+                path: path.resolve(__dirname, 'file'),
+            },
+        },
+    },
+];
+
+module.exports = routes;
